fe8ea0cc9e162f28fc6247f1f336d946d53d9285
Greg Magarshak <greg@qbix.com>
2014-01-31 12:54:12 -0500

    Refactored Q/textfill jquery plugin

---
+++ platform/plugins/Q/web/js/fn/textfill.js
@@ -0,0 +1,43 @@
+(function ($, window, document, undefined) {
+
+/*
+ * Activates placeholder effect on any input and textarea elements contained within this jquery
+ */
+Q.Tool.jQuery('Q/textfill',
+
+function (options) {
+
+	return this.refresh(options);
+
+}, 
+
+{},
+
+{
+	refresh: function (options) {
+		var ourElement, ourText = "";
+		$('*:visible', this).each(function () {
+			var $t = $(this);
+			if (!$t.children().length && $t.text().length > ourText.length) {
+				ourElement = $t;
+				ourText = $t.text();
+			}
+		});
+		var fontSize = options.maxFontPixels || (ourElement.height() + 10);
+		var maxHeight = $(this).innerHeight();
+		var maxWidth = $(this).innerWidth();
+		var textHeight;
+		var textWidth;
+		do {
+		    ourElement.css('font-size', fontSize);
+		    textHeight = ourElement.outerHeight(true);
+		    textWidth = ourElement.outerWidth(true);
+		    fontSize = fontSize - 1;
+		} while ((textHeight > maxHeight || textWidth > maxWidth) && fontSize > 3);
+		return this;
+	}
+}
+
+);
+
+})(window.jQuery, window, document);
