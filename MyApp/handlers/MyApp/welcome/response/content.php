b73044087c97a0ac863fa31ec84ca247bcb6320f
Greg Magarshak <greg@qbix.com>
2014-02-03 18:02:58 -0500

    Added handler for MyApp/home which Users/activate redirects to

--- MyApp/handlers/MyApp/welcome/response/content.php
+++ MyApp/handlers/MyApp/welcome/response/content.php
@@ -2,9 +2,9 @@
 
 function MyApp_welcome_response_content($params)
 {
-        // Do controller stuff here. Prepare variables
-        $tabs = array("foo" => "bar");
-        $description = "this is a description";
-        return Q::view('MyApp/content/welcome.php', compact('tabs', 'description'));
+	// Do controller stuff here. Prepare variables
+	$tabs = array("foo" => "bar");
+	$description = "this is a description";
+	return Q::view('MyApp/content/welcome.php', compact('tabs', 'description'));
 }
 
