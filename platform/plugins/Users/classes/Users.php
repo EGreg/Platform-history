7b00e8f657250d96daafdee6abda9c04763e416a
Greg Magarshak <greg@qbix.com>
2014-02-09 21:15:48 -0500

    The user id is used for the user url if the username is empty

--- platform/plugins/Users/classes/Users.php
+++ platform/plugins/Users/classes/Users.php
@@ -843,7 +843,8 @@ abstract class Users extends Base_Users
 		$url_parts = parse_url(Q_Request::baseUrl());
 		if (isset($url_parts['host'])) {
 			// By default, the user's url would be this:
-			$user->url = "http://$username.".$url_parts['host'];
+			$part = $username ? $username : $user->id;
+			$user->url = "http://$part.".$url_parts['host'];
 		}
 		/**
 		 * @event Users/insertUser {before}
