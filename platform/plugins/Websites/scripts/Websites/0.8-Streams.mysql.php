1785755915b8fe561fa686d255596375f240e8ac
Greg Magarshak <greg@qbix.com>
2014-03-13 17:27:59 -0400

    Added icon for Websites/header stream

--- platform/plugins/Websites/scripts/Websites/0.8-Streams.mysql.php
+++ platform/plugins/Websites/scripts/Websites/0.8-Streams.mysql.php
@@ -22,7 +22,7 @@ function Websites_0_8_Streams_mysql()
 		"Streams/image/" => array('type' => "Streams/template", "title" => "", "icon" => "default", "content" => "", "deletable" => true),
 		"Websites/bio/" => array('type' => "Streams/template", "title" => "", "icon" => "default", "content" => ""),
 		"Websites/seo/" => array('type' => "Websites/template", "title" => "Website SEO", "icon" => Q_Html::themedUrl("plugins/Websites/img/seo"), "content" => "", "deletable" => true),
-		"Websites/header" => array('type' => "Streams/image/icon", "title" => "Header image", "icon" => "default", "content" => ""),
+		"Websites/header" => array('type' => "Streams/image/icon", "title" => "Header image", "icon" => Q_Html::themedUrl("plugins/Websites/img/header"), "content" => ""),
 		"Websites/slogan" => array('type' => "Streams/text/small", "title" => "Website slogan", "icon" => "default", "content" => "The coolest website"),
 		"Websites/title" => array('type' => "Streams/text/small", "title" => "Website title", "icon" => "default", "content" => "Website Title"),
 		"Websites/menu" => array('type' => "Streams/category", "title" => "Website Menu", "icon" => "default", "content" => ""),
