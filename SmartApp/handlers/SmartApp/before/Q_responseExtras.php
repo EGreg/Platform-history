517938d8d42146900053d49e2d310cb2863ca799
Greg Magarshak <greg@qbix.com>
2014-06-12 03:15:50 -0400

    Renamed event hook files in templates to set a precedent

---
+++ SmartApp/handlers/SmartApp/before/Q_responseExtras.php
@@ -0,0 +1,40 @@
+<?php
+
+function SmartApp_before_Q_responseExtras()
+{
+	$app = Q_Config::expect('Q', 'app');
+	
+	if (Q_Config::get('Q', 'firebug', false)){
+		Q_Response::addScript("https://getfirebug.com/firebug-lite-debug.js");
+	}
+	
+	Q_Response::addScript('plugins/Q/js/iscroll.js');
+	Q_Response::addscript('plugins/Q/js/QTools.js');
+	
+	Q_Response::addStylesheet('plugins/Q/css/Ui.css');
+	Q_Response::addStylesheet('css/html.css');
+	Q_Response::addStylesheet('css/iphone.css');
+	
+	Q_Response::addScript('js/SmartApp.js');
+	
+	header('Vary: User-Agent');
+
+	if (Q_Request::isTablet()) {
+		Q_Response::addStylesheet('plugins/Q/css/tablet.css');
+	} else if (Q_Request::isMobile()) {
+		Q_Response::addStylesheet('plugins/Q/css/mobile.css');
+	} else {
+		Q_Response::addStylesheet('plugins/Q/css/desktop.css');
+	}
+	
+	Q_Response::setScriptData('Q.Layout.use', true);
+	
+	// running an event for loading action-specifig extras (if there are any)
+	$uri = Q_Dispatcher::uri();
+	$module = $uri->module;
+	$action = $uri->action;
+	$event = "$module/$action/response/responseExtras";
+	if (Q::canHandle($event)) {
+		Q::event($event);
+	}
+}
