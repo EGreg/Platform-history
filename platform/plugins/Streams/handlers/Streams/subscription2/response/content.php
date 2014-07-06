c4466b0722786268a518ac0a3fbd26844987c276
vlynev <v.lynev@gmail.com>
2014-07-06 16:57:04 +0300

    delete old code

--- platform/plugins/Streams/handlers/Streams/subscription2/response/content.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function Streams_subscription2_response_content () {
-	Q_Response::addScript("plugins/Streams/js/Streams.js");
-	Q_Response::addScript("plugins/Streams/js/tools/subscription2.js");
-
-	return Q::tool('Streams/subscription2', compact('publisherId', 'streamName'));
-}
--- platform/plugins/Streams/handlers/Streams/subscription2/response/dialog.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function Streams_subscription2_response_dialog () {
-	Q_Response::addScript("plugins/Streams/js/Streams.js");
-	Q_Response::addScript("plugins/Streams/js/tools/subscription2.js");
-
-	return Q::event('Streams/subscription2/response/content');
-}
