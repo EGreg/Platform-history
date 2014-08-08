f67b6efb0f766a84b82ad20e1edf66b6843370b1
Greg Magarshak <greg@qbix.com>
2014-08-08 01:17:49 -0400

    Fixed a few small things related to Streams/access tool

--- platform/plugins/Streams/handlers/Streams/after/Q/responseExtras.php
+++ platform/plugins/Streams/handlers/Streams/after/Q/responseExtras.php
@@ -2,6 +2,7 @@
 
 function Streams_after_Q_responseExtras() {
 	if ($preloaded = Streams_Stream::$preloaded) {
-		Q_Response::setScriptData('Q.plugins.Streams.Stream.preloaded', Db::exportArray($preloaded));
+		$preloaded = Db::exportArray($preloaded);
+		Q_Response::setScriptData('Q.plugins.Streams.Stream.preloaded', $preloaded);
 	}
 }
