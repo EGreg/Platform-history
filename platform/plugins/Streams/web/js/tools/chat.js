030d97e735ab4f53c78b9ba69df08c810cb60a0b
Greg Magarshak <greg@qbix.com>
2014-08-22 15:45:29 -0400

    Now, Streams/chat tools will update to latest at the proper time

--- platform/plugins/Streams/web/js/tools/chat.js
+++ platform/plugins/Streams/web/js/tools/chat.js
@@ -45,6 +45,9 @@ Q.Tool.define('Streams/chat', function(options) {
 	tool.refresh(function () {
 		tool.scrollToBottom();
 	});
+	Streams.refresh.beforeRequest.add(function () {
+		this.stream.refresh({messages: true});
+	}, tool);
 	//tool.Q.onInit.set(_init, "Streams/chat");
 }, 
 
