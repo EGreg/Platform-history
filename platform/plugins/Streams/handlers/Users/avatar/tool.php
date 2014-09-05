cb3779efb87bad674d88b686397abaf971c152fc
Greg Magarshak <greg@qbix.com>
2014-09-04 20:50:18 -0400

    Added iconAttributes to Users/avatar tool

--- platform/plugins/Streams/handlers/Users/avatar/tool.php
+++ platform/plugins/Streams/handlers/Users/avatar/tool.php
@@ -8,6 +8,7 @@
  *   "userId" => The user's id. Defaults to id of the logged-in user, if any.
  *   "icon" => Optional. Render icon before the display name. Can be true or a valid icon size: 40, 50 or 80.
  *   "short" => Optional. Renders the short version of the display name.
+ *   "iconAttributes" => Optional. Array of attributes to render for the icon.
  */
 function Users_avatar_tool($options)
 {
@@ -29,12 +30,23 @@ function Users_avatar_tool($options)
 	$result = '';
 	if ($icon = $options['icon']) {
 		if ($icon === true) $icon = 40;
-		$result .= Q_Html::img("plugins/Users/img/icons/{$avatar->icon}/$icon.png", 'user icon', array(
+		$attributes = array(
 			'class' => 'Users_avatar_icon'
-		));
+		);
+		$attributes = isset($options['iconAttributes'])
+			? $options['iconAttributes']
+			: array();
+		$attributes['class'] = isset($attributes['class'])
+			? $attributes['class'] . ' Users_avatar_icon'
+			: 'Users_avatar_icon';
+		$result .= Q_Html::img(
+			"plugins/Users/img/icons/{$avatar->icon}/$icon.png", 
+			'user icon', $attributes
+		);
 	}
 	$o = $options['short'] ? array('short' => true) : array();
 	$result .= '<span class="Users_avatar_name">' . $avatar->displayName($o) . '</span>';
+	unset($options['iconAttributes']);
 	Q_Response::setToolOptions($options);
 	return $result;
 }
