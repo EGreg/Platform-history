a2e87111dbdc38d26c96036eca9a343bc7f66e6e
Greg Magarshak <greg@qbix.com>
2014-10-02 11:40:04 -0400

    Improved color output when logging exceptions

--- platform/handlers/Q/exception/native.php
+++ platform/handlers/Q/exception/native.php
@@ -15,6 +15,21 @@ function Q_exception_native($params)
 	} else {
 		$trace_string = $exception->getTraceAsString();
 	}
+
+	$colors = Q_Config::get('Q', 'exception', 'colors', array());
+	Q::autoload('Q_Utils');
+	$fields = array(
+		'message' => $message,
+		'fileAndLine' => "in $file ($line)",
+		'trace' => $trace_string
+	);
+	foreach ($fields as $f => $v) {
+		$c0 = isset($colors[$f][0]) ? $colors[$f][0] : null;
+		$c1 = isset($colors[$f][1]) ? $colors[$f][1] : null;
+		$fields[$f] = Q_Utils::colored($v, $c0, $c1);
+	}
+	$colorful = "$fields[message]\n\n$fields[fileAndLine]\n$fields[trace]\n";
+
 	if ($is_ajax = Q_Request::isAjax()) {
 		// Render a JSON layout for ajax
 		switch (strtolower($is_ajax)) {
@@ -29,44 +44,26 @@ function Q_exception_native($params)
 		}
 	} else {
 		if (Q::textMode()) {
-			$colors = Q_Config::get('Q', 'exception', 'colors', array());
-			Q::autoload('Q_Utils');
-			$fields = array(
-				'message' => $message,
-				'fileAndLine' => "in $file ($line)",
-				'trace' => $trace_string
-			);
-			foreach ($fields as $f => $v) {
-				$c0 = isset($colors[$f][0]) ? $colors[$f][0] : null;
-				$c1 = isset($colors[$f][1]) ? $colors[$f][1] : null;
-				$fields[$f] = Q_Utils::colored($v, $c0, $c1);
-			}
-			
-			$content = "$fields[message]\n$fields[fileAndLine]\n$fields[trace]";
-			echo $content;
-			echo "\n";
+			echo $colorful;
 			exit;
-		} else {
-			if (($exception instanceof Q_Exception_PhpError) or !empty($exception->messageIsHtml)) {
-				// do not sanitize $message
-			} else {
-				$message = Q_Html::text($message);
-			}
-			$content = "<h1 class='exception_message'>$message</h1>";
-			if (Q_Config::get('Q', 'exception', 'showFileAndLine', true)) {
-				$content .= "<h3 class='exception_fileAndLine'>in $file ($line)</h3>";
-			}
-			if (Q_Config::get('Q', 'exception', 'showTrace', true)) {
-				$content .= "<pre class='exception_trace'>$trace_string</pre>";
-			}
-			$content .= str_repeat(' ', 512); // because of chrome
-			$title = "Exception occurred";
-			$dashboard = "";
-			echo Q::view('Q/layout/html.php', compact('content', 'dashboard', 'title'));
 		}
+		if (($exception instanceof Q_Exception_PhpError) or !empty($exception->messageIsHtml)) {
+			// do not sanitize $message
+		} else {
+			$message = Q_Html::text($message);
+		}
+		$content = "<h1 class='exception_message'>$message</h1>";
+		if (Q_Config::get('Q', 'exception', 'showFileAndLine', true)) {
+			$content .= "<h3 class='exception_fileAndLine'>in $file ($line)</h3>";
+		}
+		if (Q_Config::get('Q', 'exception', 'showTrace', true)) {
+			$content .= "<pre class='exception_trace'>$trace_string</pre>";
+		}
+		$content .= str_repeat(' ', 512); // because of chrome
+		$title = "Exception occurred";
+		$dashboard = "";
+		echo Q::view('Q/layout/html.php', compact('content', 'dashboard', 'title'));
 	}
 	$app = Q_Config::get('Q', 'app', null);
-	Q::log("$app: Exception in " . ceil(Q::milliseconds()) . "ms\n" );
-	Q::log("$message\n  in $file ($line)");
-	Q::log("$trace_string\n");
+	Q::log("$app: Exception in " . ceil(Q::milliseconds()) . "ms:\n\n".$colorful);
 }
