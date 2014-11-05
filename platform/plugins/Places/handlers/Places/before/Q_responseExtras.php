b012ebec17b43ee57f70ea7b4bc21318f20594db
Greg Magarshak <greg@qbix.com>
2014-11-05 11:21:36 -0500

    Fixed a small typo

--- platform/plugins/Places/handlers/Places/before/Q_responseExtras.php
+++ platform/plugins/Places/handlers/Places/before/Q_responseExtras.php
@@ -4,7 +4,7 @@ function Places_before_Q_responseExtras()
 {
 	Q_Response::addScript('plugins/Places/js/Places.js');
 	Q_Response::addStylesheet("plugins/Places/css/Places.css");
-	if ($key = Q_Config::get('Places', 'google', 'key', 'web', null)) {
+	if ($key = Q_Config::get('Places', 'google', 'keys', 'web', null)) {
 		Q_Response::setScriptData("Q.Places.loadGoogleMaps.key", $key);
 	}
 }
