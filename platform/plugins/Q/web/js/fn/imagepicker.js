a2c9039eb61b1a98be0c795027a4640859917fca
Greg Magarshak <greg@qbix.com>
2015-01-28 04:04:48 -0500

    Workaround for IOS7 subsampling bug

--- platform/plugins/Q/web/js/fn/imagepicker.js
+++ platform/plugins/Q/web/js/fn/imagepicker.js
@@ -186,8 +186,8 @@ Q.Tool.jQuery('Q/imagepicker', function _Q_imagepicker(o) {
 				    var destHeight = coord.requiredSize.height;
 				    var destLeft = 0;
 				    var destTop = 0;
-				    context.drawImage(
-						$img[0],
+				    drawImageIOSFix(
+						context, $img[0],
 						sourceLeft, sourceTop, sourceWidth, sourceHeight,
 						destLeft, destTop, destWidth, destHeight
 					);
@@ -212,7 +212,7 @@ Q.Tool.jQuery('Q/imagepicker', function _Q_imagepicker(o) {
                     var $croppingElement = $('<img />').attr({ src: img.src })
 						.css({'visibility': 'hidden'});
                     Q.Dialogs.push({
-                        className: 'Q_Dialog_imagepicker',
+                        className: 'Q_dialog_imagepicker',
                         title: state.croppingTitle,
                         content: $croppingElement,
                         destroyOnClose: true,
@@ -427,6 +427,51 @@ Q.Tool.jQuery('Q/imagepicker', function _Q_imagepicker(o) {
 			 }
 		});
 	}
+	
+   /**
+    * Detecting vertical squash in loaded image.
+    * Fixes a bug which squash image vertically while drawing into canvas for some images.
+    * This is a bug in iOS6 devices. This function from https://github.com/stomita/ios-imagefile-megapixel
+    * 
+    */
+   function detectVerticalSquash(img) {
+       var iw = img.naturalWidth, ih = img.naturalHeight;
+       var canvas = document.createElement('canvas');
+       canvas.width = 1;
+       canvas.height = ih;
+       var ctx = canvas.getContext('2d');
+       ctx.drawImage(img, 0, 0);
+       var data = ctx.getImageData(0, 0, 1, ih).data;
+       // search image edge pixel position in case it is squashed vertically.
+       var sy = 0;
+       var ey = ih;
+       var py = ih;
+       while (py > sy) {
+           var alpha = data[(py - 1) * 4 + 3];
+           if (alpha === 0) {
+               ey = py;
+           } else {
+               sy = py;
+           }
+           py = (ey + sy) >> 1;
+       }
+       var ratio = (py / ih);
+       return (ratio===0)?1:ratio;
+   }
+
+   /**
+    * A replacement for context.drawImage
+    * (args are for source and destination).
+    */
+   function drawImageIOSFix(ctx, img, sx, sy, sw, sh, dx, dy, dw, dh) {
+       var vertSquashRatio = detectVerticalSquash(img);
+    // Works only if whole image is displayed:
+    // ctx.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh / vertSquashRatio);
+    // The following works correct also when only a part of the image is displayed:
+       ctx.drawImage(img, sx * vertSquashRatio, sy * vertSquashRatio, 
+                          sw * vertSquashRatio, sh * vertSquashRatio, 
+                          dx, dy, dw, dh );
+   }
 },
 
 {
