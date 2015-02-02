677c9986d481eeb04bc5edadd2db38c02d3e3430
Greg Magarshak <greg@qbix.com>
2015-02-01 22:37:09 -0500

    Stoped showing trigger when drawer is not visible

--- platform/plugins/Q/web/js/tools/drawers.js
+++ platform/plugins/Q/web/js/tools/drawers.js
@@ -457,7 +457,9 @@ Q.Tool.define("Q/drawers", function _Q_drawers(options) {
 				canShowTrigger = true;
 			});
 			state.$interval = setInterval(function () {
-				if (canShowTrigger && state.$scrolling.scrollTop() === 0) {
+				if (canShowTrigger
+				&& state.$drawers.eq(0).is(':visible')
+				&& state.$scrolling.scrollTop() === 0) {
 					state.$trigger.show();
 				}
 			}, 300);
