0e994ded82287b90a498c288ccab01638cbadf00
Greg Magarshak <greg@qbix.com>
2015-05-27 16:03:45 -0400

    Added css files

---
+++ platform/plugins/Q/web/css/inplace.css
@@ -0,0 +1,26 @@
+.Q_inplace_tool, .Q_inplace_tool_container { display: inline-block; position: relative; }
+.Q_inplace_tool .Q_inplace_tool_editbuttons { display: none; position: absolute; left: 0px; z-index: 9901; }
+.Q_inplace_tool .Q_inplace_tool_editbuttons button { display: inline; color: black !important; }
+.Q_inplace_tool .Q_inplace_tool_static { display: inline-block; border: solid 1px transparent; cursor: default; }
+.Q_inplace_tool .Q_inplace_tool_blockstatic { display: block; border: solid 1px transparent; cursor: default; }
+.Q_inplace_tool .Q_hover .Q_inplace_tool_static,
+.Q_inplace_tool .Q_hover .Q_inplace_tool_blockstatic { border: dashed 1px #AAE; cursor: pointer; }
+.Q_inplace_tool .Q_hover .Q_inplace_tool_editbuttons { background-color: #EEE; display: inline; }
+.Q_inplace_tool .Q_inplace_tool_fieldinput { max-height: 500px; }
+.Q_inplace_tool .Q_inplace_tool_undermessage { display: none; position: absolute; bottom: -18px; left: 10px; min-width: 100px; }
+.Q_inplace_tool .Q_error { background-color: #FEE; }
+
+.Q_inplace_tool .Q_inplace_tool_form { display: none; }
+.Q_inplace_tool .Q_inplace_tool_invisible { display: none; }
+.Q_inplace_tool .Q_editing .Q_inplace_tool_form { display: inline; margin: 0px; }
+.Q_inplace_tool .Q_editing .Q_inplace_tool_form * { margin: 0px; }
+.Q_inplace_tool .Q_editing .Q_inplace_tool_static { display: none; }
+.Q_inplace_tool .Q_editing .Q_inplace_tool_blockstatic { display: none; }
+.Q_inplace_tool .Q_editing .Q_inplace_tool_edit { display: none; }
+.Q_inplace_tool .Q_editing .Q_inplace_tool_invisible { display: block; visibility: hidden; position: absolute; top: -20px; z-index: 1; }
+.Q_inplace_tool .Q_inplace_tool_buttons { position: absolute; left: 0px; z-index: 9900; min-width: 200px; line-height: 1px; text-align: left !important; color: black !important; }
+.Q_inplace_tool .Q_inplace_tool_buttons button { background-color: #EEE; display: inline; color: black !important; }
+.Q_inplace_tool .Q_nocancel button.Q_inplace_tool_cancel { display: none; }
+
+* html .Q_inplace_tool .Q_inplace_tool_editbuttons { top: -17px; }
+* html .Q_inplace_tool .Q_inplace_tool_buttons { top: -17px; }
