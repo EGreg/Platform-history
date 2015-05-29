b00a89344d5c7fdb8f54b55188ae0ce91ac305cb
Greg Magarshak <greg@qbix.com>
2015-05-29 01:39:28 -0400

    Updated Websites and Streams plugins to install symlinks

--- platform/plugins/Websites/scripts/Websites/0.8-Users.mysql.php
+++ platform/plugins/Websites/scripts/Websites/0.8-Users.mysql.php
@@ -7,7 +7,17 @@ function Websites_0_8_Users_mysql()
 		throw new Q_Exception('Websites: Please fill in the config field "Websites"/"user"/"id"');
 	}
 	
-	Users_Label::addLabel('Websites/admins', $userId, 'Website Admins');
+	Users_Label::addLabel('Websites/admins', $userId, 'Website Admins', 'Website/admins');
+	
+	$cwd = getcwd();
+	chdir(USERS_PLUGIN_FILES_DIR.DS.'Users'.DS.'icons');
+	if (!file_exists('Websites')) {
+		symlink(
+			WEBSITES_PLUGIN_FILES_DIR.DS.'Websites'.DS.'icons'.DS.'labels'.DS.'Websites',
+			'Websites'
+		);
+	}
+	chdir($cwd);
 }
 
 Websites_0_8_Users_mysql();
