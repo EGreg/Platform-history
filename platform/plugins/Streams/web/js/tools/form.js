EGreg/Platform-history

commit 173a7236aa79f723a3b2bc89c5998210a41be255

Fixed saving streams via Streams/form

platform/plugins/Streams/web/js/tools/form.js
@@ -33,10 +33,11 @@ Q.Tool.define("Streams/form", function (options) {
 		var $parent = $this.parent();
 		var name = null;
 		var value = null;
+		var $inputs = $input;
 		if ($parent.attr('data-type') === 'date') {
 			var notReady = false;
 			var y, m, d;
-			$parent.find('select').each(function () {
+			$inputs = $parent.find('select').each(function () {
 				var $this = $(this);
 				switch ($this.attr('name').split('_').pop()) {
 					case 'year': y = $this.val(); break;
@@ -62,8 +63,9 @@ Q.Tool.define("Streams/form", function (options) {
 		var fields = {};
 		fields[name] = value;
 		fields.inputs = $hidden.val();
+		$inputs.addClass('Q_uploading');
 		Q.req('Streams/form', 'streams', function () {
-			debugger;
+			$inputs.removeClass('Q_uploading');
 		}, {method: 'post', fields: fields});
 	});
 

platform/plugins/Streams/classes/Streams.php
@@ -777,8 +777,7 @@ abstract class Streams extends Base_Streams
 			$p = new Q_Tree();
 			$p->load(STREAMS_PLUGIN_CONFIG_DIR.DS.'streams.json');
 			$p->load(APP_CONFIG_DIR.DS.'streams.json');
-			$info = $p->get($fields['name'], array());
-			if (!empty($info)) {
+			if ($info = $p->get($fields['name'], array())) {
 				foreach (Base_Streams_Stream::fieldNames() as $f) {
 					if (isset($info[$f])) {
 						$stream->$f = $info[$f];
@@ -792,6 +791,7 @@ abstract class Streams extends Base_Streams
 
 		// extend with any config defaults for this stream type
 		$fieldNames = Streams::getExtendFieldNames($type);
+		$fieldNames[] = 'name';
 		$defaults = Q_Config::get('Streams', 'types', $type, 'defaults', array());
 		foreach ($fieldNames as $f) {
 			if (isset($fields[$f])) {
@@ -801,7 +801,7 @@ abstract class Streams extends Base_Streams
 			}
 		}
 		
-		// handle settinfg of attributes
+		// handle setting of attributes
 		if (isset($fields['attributes']) and is_array($fields['attributes'])) {
 			$stream->setAttribute($fields['attributes']);
 			unset($fields['attributes']);

platform/plugins/Streams/handlers/Streams/form/post.php
@@ -21,7 +21,7 @@ function Streams_form_post($params = array())
 			));
 		}
 		list($streamExists, $publisherId, $streamName, $fieldName) = $info;
-		$stream = Streams::fetchOne(null, $publisherId, $streamName, $fieldName);
+		$stream = Streams::fetchOne(null, $publisherId, $streamName);
 		if (!$stream) {
 			if ($user->id !== $publisherId
 			or !Q_Config::get('Streams', 'possibleUserStreams', $streamName, false)) {

commit 01ace56f862008a23076865c1a9daf83f74f5e11

Fixed a typo introduced in commit 173a7236aa79f723a3b2bc89c5998210a41be255

platform/plugins/Streams/web/js/tools/form.js
@@ -33,7 +33,7 @@ Q.Tool.define("Streams/form", function (options) {
 		var $parent = $this.parent();
 		var name = null;
 		var value = null;
-		var $inputs = $input;
+		var $inputs = $this;
 		if ($parent.attr('data-type') === 'date') {
 			var notReady = false;
 			var y, m, d;