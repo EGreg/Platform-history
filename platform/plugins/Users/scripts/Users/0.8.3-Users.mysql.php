e3499525700f36de0a7176a2de47a9b218ed11b6
Greg Magarshak <greg@qbix.com>
2015-08-20 13:59:05 -0400

    Slightly expanded Users plugin install script

--- platform/plugins/Users/scripts/Users/0.8.3-Users.mysql.php
+++ platform/plugins/Users/scripts/Users/0.8.3-Users.mysql.php
@@ -5,6 +5,9 @@ function Users_0_8_3_Users_mysql()
 	$app = Q_Config::expect('Q', 'app');
 	$user = new Users_User();
 	$user->id = $app;
+	$user->username = $app;
+	$user->url = Q_Config::expect('Q', 'web', 'appRootUrl');
+	$user->signedUpWith = 'none';
 	$user->save();
 }
 
