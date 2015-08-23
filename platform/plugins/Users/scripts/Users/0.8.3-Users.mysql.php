91f6a64ce6a14dab062a453c5a3114a8e0e78154
Greg Magarshak <greg@qbix.com>
2015-08-23 02:48:42 -0400

    Slightly improved Users plugin install script

--- platform/plugins/Users/scripts/Users/0.8.3-Users.mysql.php
+++ platform/plugins/Users/scripts/Users/0.8.3-Users.mysql.php
@@ -3,10 +3,13 @@
 function Users_0_8_3_Users_mysql()
 {
 	$app = Q_Config::expect('Q', 'app');
+	$appRootUrl = Q_Config::expect('Q', 'web', 'appRootUrl');
+	
 	$user = new Users_User();
 	$user->id = $app;
 	$user->username = $app;
-	$user->url = Q_Config::expect('Q', 'web', 'appRootUrl');
+	$user->url = $appRootUrl;
+	$user->icon = "$appRootUrl/img/icon";
 	$user->signedUpWith = 'none';
 	$user->save();
 }
