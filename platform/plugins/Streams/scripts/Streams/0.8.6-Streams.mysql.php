aa8b547c5f3974ed753e5a3c548a7aa09cee76d0
Greg Magarshak <greg@qbix.com>
2015-10-13 20:33:59 -0400

    Added access template for admins to manage Streams/category streams

--- platform/plugins/Streams/scripts/Streams/0.8.6-Streams.mysql.php
+++ platform/plugins/Streams/scripts/Streams/0.8.6-Streams.mysql.php
@@ -4,10 +4,21 @@ function Streams_0_8_6_Streams_mysql()
 {
 	$app = Q_Config::expect('Q', 'app');
 	
-	// access for managing app contacts
+	// access for managing communities
 	$access = new Streams_Access();
 	$access->publisherId = $app;
 	$access->streamName = 'Streams/community*';
+	$access->ofUserId = '';
+	$access->ofContactLabel = "$app/admins";
+	$access->readLevel = Streams::$READ_LEVEL['messages'];
+	$access->writeLevel = Streams::$WRITE_LEVEL['edit'];
+	$access->adminLevel = Streams::$ADMIN_LEVEL['manage'];
+	$access->save();
+	
+	// access for managing categories
+	$access = new Streams_Access();
+	$access->publisherId = $app;
+	$access->streamName = 'Streams/category/';
 	$access->ofUserId = '';
 	$access->ofContactLabel = "$app/admins";
 	$access->readLevel = Streams::$READ_LEVEL['messages'];
