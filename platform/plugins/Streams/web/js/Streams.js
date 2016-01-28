e74bb365a406eed59e5f0f4d9e14b9f22e319a1c
Greg Magarshak <greg@qbix.com>
2016-01-28 23:51:03 +0200

    Updated the PHP version of Streams/inplace tool

--- platform/plugins/Streams/web/js/Streams.js
+++ platform/plugins/Streams/web/js/Streams.js
@@ -189,7 +189,7 @@ Streams.iconUrl = function(icon, size) {
 	}
 	size = (String(size).indexOf('.') >= 0) ? size : size+'.png';
 	var src = (icon + '/' + size).interpolate({
-		"{{baseUrl}}": Q.info.baseUrl
+		"baseUrl": Q.info.baseUrl
 	});
 	return src.isUrl() ? src : Q.url('plugins/Streams/img/icons/'+src);
 };
