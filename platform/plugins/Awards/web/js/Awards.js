09082818893c368db901a92e231ce3951196874c
Aleksei Tcelishchev <casualuser@gmail.com>
2016-01-29 21:37:45 +0700

    Awards v0.4 required modifications

--- platform/plugins/Awards/web/js/Awards.js
+++ platform/plugins/Awards/web/js/Awards.js
@@ -6,11 +6,42 @@ Q.Awards = Q.plugins.Awards = {};
 	
 	Streams.onMessage('Awards/credits', "").set(function (data) {
 		
+		var amount = 199;
+
 		Awards.amount = amount;
 		Awards.onCredits.handle(amount);
 		
 	});
-	
+
+	Q.page('', function () {
+
+		$('.Awards_pay').on(Q.Pointer.click, function () {
+
+			var fields = '';
+			Q.req(
+				'Awards/pay', // uri - string of the form
+				'results', // slotNames
+				// callback
+				function () {
+				},
+				// A hash of options, to be passed to Q.request
+				{
+					method: 'post',
+					fields: fields
+				}
+			);
+
+			console.log('button clicked');
+			return false;
+		});
+
+	});
+
+//	Streams.onMessage('Awards/credits', "").set(function (data) {
+//		Awards.amount = amount;
+//		Awards.onCredits.handle(amount);
+//	});
+
 	Q.onReady.set(function () {
 		Awards.onCredits.handle(Q.plugins.Awards.credits.amount);
 	}, 'Awards');
