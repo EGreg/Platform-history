c7ed6cc1f12ce80d010377a016fdc55b33eea673
Aleksei Tcelishchev <casualuser@gmail.com>
2016-02-02 00:27:13 +0700

    refactored Awards\pay tool methods

--- platform/plugins/Awards/web/js/Awards.js
+++ platform/plugins/Awards/web/js/Awards.js
@@ -1,36 +1,36 @@
-Q.Awards = Q.plugins.Awards = {};
+Q.Awards = Q.plugins.Awards = {
 
-(function(Q, Awards, Streams, $) {
+	paymentDialog: function (callback, options) {
+		Q.Dialogs.push(Q.extend({
+			title: '',
+			content:
+			'<iframe ' +
+			'name="authnet" ' +
+			'src="" ' +
+			'width="480" ' +
+			'height="640" ' +
+			'frameborder="0" ' +
+			'scrolling="no" ' +
+			'id="authnet" ' +
+			'class="authnet" ' +
+			'></iframe>'
+		}, options));
 
-	Awards.onCredits = new Q.Event();
-	
-	Streams.onMessage('Awards/credits', "").set(function (data) {
-		
-		var amount = 199;
-
-		Awards.amount = amount;
-		Awards.onCredits.handle(amount);
-		
-	});
-
-	Q.page('', function () {
-
-		$('.Awards_auth').on(Q.Pointer.click, function () {
-			Q.Dialogs.push({
-				title: '',
-				content:
-				'<iframe ' +
-				'name="authnet" ' +
-				'src="" ' +
-				'width="480" ' +
-				'height="640" ' +
-				'frameborder="0" ' +
-				'scrolling="no" ' +
-				'id="authnet" ' +
-				'class="authnet" ' +
-				'></iframe>'
-			});
-		});
+		function subscribe() {
+			var fields = '';
+			Q.req(
+				'Awards/pay', // uri - string of the form
+				'payment', // slotNames
+				// callback
+				function () {
+				},
+				// A hash of options, to be passed to Q.request
+				{
+					method: 'post',
+					fields: fields
+				});
+			Q.Dialogs.pop();
+		};
 
 		$('.Awards_confirm').on(Q.Pointer.click, function () {
 			Q.Dialogs.push({
@@ -54,24 +54,21 @@ Q.Awards = Q.plugins.Awards = {};
 				};
 			});
 		});
+	}
+};
+
+(function(Q, Awards, Streams, $) {
+
+	Awards.onCredits = new Q.Event();
+	
+	Streams.onMessage('Awards/credits', "").set(function (data) {
+		
+		var amount = 199;
+
+		Awards.amount = amount;
+		Awards.onCredits.handle(amount);
 
 	});
-
-	function subscribe() {
-		var fields = '';
-		Q.req(
-			'Awards/pay', // uri - string of the form
-			'results', // slotNames
-			// callback
-			function () {
-			},
-			// A hash of options, to be passed to Q.request
-			{
-				method: 'post',
-				fields: fields
-			});
-		Q.Dialogs.pop();
-	};
 
 	Q.Tool.define({
 		"Awards/pay"           : "plugins/Awards/js/tools/pay.js"
