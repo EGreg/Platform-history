a620c7a80662e96c4e984449da3e39cb766572f0
Aleksei Tcelishchev <casualuser@gmail.com>
2016-02-02 18:51:00 +0700

    required tweaks for refactored classes and interface code to make it works well

--- platform/plugins/Awards/views/Awards/tool/subscription/authnet.php
+++ platform/plugins/Awards/views/Awards/tool/subscription/authnet.php
@@ -1,5 +1,8 @@
-<?php echo Q_Html::form('https://test.authorize.net/profile/manage', 'POST') ?> 
+<?php echo Q_Html::form('https://test.authorize.net/profile/manage', 'POST') ?>
+
 	<?php echo Q_Html::hidden(array('Token' => $token ))?>
+
 	<input class="Q_button Awards_auth" type="submit" value="Payment info"/>
 	<button class="Q_button Awards_confirm"><?php echo $button ?></button>
+
 </form>
